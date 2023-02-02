e69a4e5263034f05d75d944440a5eb7d196d50c8
DenSTR <web_life@inbox.ru>
2023-02-02 11:11:27 +0300

    добавлен хедер и футер

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,8 +4,13 @@ import './index.css';
 export default function Header() {
   return (
     <header>
-      <div>
+      <div className='top'>
         <span className='logo'>House staff</span>
+        <ul className='nav'>
+          <li>Про нас</li>
+          <li>Контакты</li>
+          <li>Кабинет</li>
+        </ul>
       </div>
       <div className='presentation'></div>
     </header>
