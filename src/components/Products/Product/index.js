47a150f32ebef850a585e4597c5e265aa7cf6850
DenSTR <web_life@inbox.ru>
2023-02-02 13:21:34 +0300

    выводит список продуктов

--- src/components/Products/Product/index.js
+++ src/components/Products/Product/index.js
@@ -1,15 +1,20 @@
 import React from "react";
+import './index.css';
 
 export default class Product extends React.Component {
   render() {
     const { title, image, description, price } = this.props.product;
     return(
       <li className='product'>
-        <h2>{title}</h2>
-        <img src={image} alt={title}/>
-        <p>{description}</p>
-        <div>{price}$</div>
-        <div role='button' className='add-to-cart'>+</div>
+        <h2 className='title'>{title}</h2>
+        <picture>
+          <img src={image} alt={title}/>
+        </picture>
+        <p className='desc'>{description}</p>
+        <div className='inner'>
+          <div className='price'>{price}$</div>
+          <div role='button' className='add-to-cart' />
+        </div>
       </li>
     )
   }
--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './index.css'
 import Product from "./Product";
 import axios from "axios";
 
